c506d4a8151cb470c9fc38edaaac9b36852b9424
Charlie Wunderlich <cww324@gmail.com>
2025-04-01 10:49:54 -0500

    changed main.css/index.html/main.js to get the functionality of GuestList.js to work.. Dom handler working, counting correct number of visitors.  Oh i also changed the database some, just to mess around with different numbers

---
+++ scripts/GuestList.js
@@ -0,0 +1,40 @@
+
+// dunno name of function to import yet
+import { getGuests } from "./database.js"
+
+const parkAreaElements = document.querySelectorAll(".park-area");
+
+const guestList = getGuests();
+
+export const GuestList = () => {
+const areaElements = document.querySelectorAll(".park-area")
+
+    areaElements.forEach(areaElement => {
+        areaElement.addEventListener("click", () => {
+            const areaId = parseInt(areaElement.getAttribute("data-id"))
+
+            const guestList = getGuests()
+            const visitorCount = guestList.filter(guest => guest.parkAreaId === areaId).length
+            alert(`There are ${visitorCount} guests in this area`)
+        })
+    })
+
+}
+
+
+
+
+
+/* old code..
+
+parkAreaElements.forEach(areaElement => {
+    areaElement.addEventListener("click", () => {
+        const areaId = parseInt(areaElement.getAttribute("data-id"))
+        const visitorCount = guestList.filter(guest => guest.parkAreaId === areaId).length
+        alert(`There are ${visitorCount} in this area`)
+    })
+})
+
+
+
+*/
--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,39 @@
 
-
+import { GuestList } from "./GuestList.js";
 
 const mainContainer = document.querySelector("#container")
 
 const applicationHTML = `
 <header> 
-<h1>Cider Falls</h1>
-<img src="" alt="Cider Logo">
+  <h1>Cider Falls</h1>
+  <img src="" alt="Cider Logo">
 </header>
-<div class="">
-    <section class="">
-        <h2>Park Areas</h2>
-        
-    </section>
-    <section class="">
-        <h2>Services</h2>
-        
-        </section>
-    <section class=""> 
-        <h2>Guests</h2>
-        
-    </section>    
-</div>
-<footer>
-<h2></h2>
-</footer>
-`
 
+<div class="">
+  <section class="">
+    <h2>Park Areas</h2>
+    <div class="park-area" data-id="1">Chamfort River</div> 
+    <div class="park-area" data-id="2">Lost Wolf Hiking Trail</div>
+    <div class="park-area" data-id="3">Lodge Area</div>
+    <div class="park-area" data-id="4">Gander River</div>
+    <div class="park-area" data-id="5">Campgrounds</div>
+    <div class="park-area" data-id="6">Pine Bluff Trails</div>
+  </section>
+
+  <section class="">
+    <h2>Services</h2>
+  </section>
+
+  <section class=""> 
+    <h2>Guests</h2>
+  </section>    
+</div>
+
+<footer>
+  <h2></h2>
+</footer>
+`;
 mainContainer.innerHTML = applicationHTML
+
+
+GuestList()
