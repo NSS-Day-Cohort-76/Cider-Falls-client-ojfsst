79f5ce07ad09c3b825516cb5ba720abb0c063c68
David West <davidpwest1220@gmail.com>
2025-04-01 14:25:08 -0500

    Fixed merge issues between mine and CW files, see descr.

--- scripts/GuestList.js
+++ scripts/GuestList.js
@@ -1,22 +1,22 @@
-import { getParkAreas, getParkGuests } from "./database.js";
-import { addParkAreaEventListener } from "./EventListener.js";
+// import { getParkAreas, getParkGuests } from "./database.js";
+// import { addParkAreaEventListener } from "./EventListener.js";
 
-export const GuestList = () => {
-  const parkAreas = getParkAreas();
-  const guests = getParkGuests();
-  const container = document.getElementById("park-areas-container");
+// export const GuestList = () => {
+//   const parkAreas = getParkAreas();
+//   const guests = getParkGuests();
+//   const container = document.getElementById("park-areas-container");
 
-  let parkAreaHTML = "";
-  for (const area of parkAreas) {
-    parkAreaHTML += `
-      <div class="park-area" data-id="${area.id}">
-        ${area.name}
-      </div>
-    `;
-  }
-  container.innerHTML = parkAreaHTML;
+//   let parkAreaHTML = "";
+//   for (const area of parkAreas) {
+//     parkAreaHTML += `
+//       <div class="park-area" data-id="${area.id}">
+//         ${area.name}
+//       </div>
+//     `;
+//   }
+//   container.innerHTML = parkAreaHTML;
 
 
 
-addParkAreaEventListener(guests)
-}
+// addParkAreaEventListener(guests)
+// }
